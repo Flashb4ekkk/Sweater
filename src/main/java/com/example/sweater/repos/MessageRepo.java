33811514b478a0ca67684e806df31e6ed0e08eb2
Bethakzs <romanwdwd@gmail.com>
2023-09-24 00:15:28 +0300

    init commit

---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
